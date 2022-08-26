0d20eb4c54fcefa052396cc4d44d00379d9fe0e1
Andy <anfernandez@fi.uba.ar>
2022-08-25 21:27:42 -0300

    adds info to reset_blueprint

--- project/blueprints/reset_blueprint.py
+++ project/blueprints/reset_blueprint.py
@@ -1,9 +1,8 @@
-from flask_openapi3 import APIBlueprint, Info
+from flask_openapi3 import APIBlueprint
 from project import db
 from flask import jsonify
 from decouple import config
 
-info = Info(title='Reset endpoint', description="Resets the DB, must be used only for testing purposes")
 
 RESET_ENDPOINT = "/database/reset"
 
@@ -11,9 +10,8 @@ EXPECTED_RESPONSES = {}
 
 reset_blueprint = APIBlueprint("reset_blueprint", __name__)
 
-@reset_blueprint.post(
-    f"{RESET_ENDPOINT}"
-)
+
+@reset_blueprint.post(f"{RESET_ENDPOINT}", description="Resets the DB (testing only!)")
 def reset_database():
     reset_allowed = config("ALLOW_DB_RESET", cast=bool, default=False)
 
